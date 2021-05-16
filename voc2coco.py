33be77e36aa19284ebdcce14d410dcb5df21c70b
Yinlin <huyinlin@gmail.com>
2021-05-16 18:27:11 +0200

    update

--- voc2coco.py
+++ voc2coco.py
@@ -45,21 +45,23 @@ def get_defined_categories(labels_file):
     list_fp.close()
     return results
 
-def convert(jpg_list, labels_file, out_dirname):
+def convert(jpg_list, labels_file, out_dirname, kitti=False):
     list_fp = open(jpg_list, 'r')
     json_dict = {"images":[], "type": "instances", "annotations": [],
                  "categories": []}
-    categories = get_defined_categories(labels_file)
+    # categories = get_defined_categories(labels_file)
+    categories = {}
     bnd_id = START_BOUNDING_BOX_ID
 
-    if not os.path.exists(out_dirname):
-        os.makedirs(out_dirname)
     for line in list_fp:
         line = line.strip()
         print("Processing %s"%(line))
-        shutil.copy2(line, out_dirname + os.sep + os.path.basename(line))
+        # if not os.path.exists(out_dirname):
+        #     os.makedirs(out_dirname)
+        # shutil.copy2(line, out_dirname + os.sep + os.path.basename(line))
+
         # xml_f = os.path.join(xml_dir, line)
-        xml_f = line.replace('/JPEGImages/', '/Annotations/').replace('.jpg', '.xml')
+        xml_f = line.replace('/JPEGImages/', '/Annotations/').replace('.jpg', '.xml').replace('.png', '.xml')
         tree = ET.parse(xml_f)
         root = tree.getroot()
         # path = get(root, 'path')
@@ -69,29 +71,33 @@ def convert(jpg_list, labels_file, out_dirname):
         #     filename = get_and_check(root, 'filename', 1).text
         # else:
         #     raise NotImplementedError('%d paths found in %s'%(len(path), line))
+        #
         filename = os.path.basename(line)
         ## The filename must be a number
         image_id = get_filename_as_int(filename)
-        size = get_and_check(root, 'size', 1)
-        width = int(get_and_check(size, 'width', 1).text)
-        height = int(get_and_check(size, 'height', 1).text)
-        image = {'file_name': filename, 'height': height, 'width': width,
-                 'id':image_id}
-        json_dict['images'].append(image)
         ## Cruuently we do not support segmentation
         #  segmented = get_and_check(root, 'segmented', 1).text
         #  assert segmented == '0'
+        objcnt = 0
         for obj in get(root, 'object'):
             category = get_and_check(obj, 'name', 1).text
-            if category not in categories:
-                new_id = len(categories)
-                categories[category] = new_id
-            category_id = categories[category]
+            if kitti:
+                if category not in ['car', 'Truck', 'Van', 'bus', 'truck']:
+                    continue
+                category = 'car'
+                category_id = 1
+            else:
+                if category not in categories:
+                    continue # only for the desired categories !!!!!!!!!!
+                    new_id = len(categories)
+                    categories[category] = new_id
+                category_id = categories[category]
+            
             bndbox = get_and_check(obj, 'bndbox', 1)
-            xmin = int(get_and_check(bndbox, 'xmin', 1).text) - 1
-            ymin = int(get_and_check(bndbox, 'ymin', 1).text) - 1
-            xmax = int(get_and_check(bndbox, 'xmax', 1).text)
-            ymax = int(get_and_check(bndbox, 'ymax', 1).text)
+            xmin = float(get_and_check(bndbox, 'xmin', 1).text) - 1
+            ymin = float(get_and_check(bndbox, 'ymin', 1).text) - 1
+            xmax = float(get_and_check(bndbox, 'xmax', 1).text)
+            ymax = float(get_and_check(bndbox, 'ymax', 1).text)
             assert(xmax > xmin)
             assert(ymax > ymin)
             o_width = abs(xmax - xmin)
@@ -102,6 +108,15 @@ def convert(jpg_list, labels_file, out_dirname):
                    'segmentation': []}
             json_dict['annotations'].append(ann)
             bnd_id = bnd_id + 1
+            objcnt += 1
+        #
+        if objcnt > 0:
+            size = get_and_check(root, 'size', 1)
+            width = int(get_and_check(size, 'width', 1).text)
+            height = int(get_and_check(size, 'height', 1).text)
+            image = {'file_name': filename, 'height': height, 'width': width,
+                    'id':image_id}
+            json_dict['images'].append(image)
 
     for cate, cid in categories.items():
         cat = {'supercategory': 'none', 'id': cid, 'name': cate}
@@ -113,14 +128,13 @@ def convert(jpg_list, labels_file, out_dirname):
     json_fp.close()
     list_fp.close()
 
-
 if __name__ == '__main__':
     # if len(sys.argv) <= 1:
     #     print('3 auguments are need.')
     #     print('Usage: %s XML_LIST.txt XML_DIR OUTPU_JSON.json'%(sys.argv[0]))
     #     exit(1)
     # convert(sys.argv[1], sys.argv[2], sys.argv[3])
-    jpgListFile = "/home/yhu/github/cross-domain-detection/datasets/watercolor/test.txt"
-    labelsFile = "/home/yhu/github/cross-domain-detection/datasets/watercolor/labels.txt"
-    outDirName = "./test"
-    convert(jpgListFile, labelsFile, outDirName)
+    jpgListFile = "/data/Cityscapes_detection/train_200shots_5.txt"
+    labelsFile = "/xx.txt"
+    outDirName = "./train_200shots_5"
+    convert(jpgListFile, labelsFile, outDirName, kitti=False)
